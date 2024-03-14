000bf3d518512ad7304298bcbc6693cbd4eef080
gabsmelin <rm550202@fiap.com.br>
2024-03-14 16:32:08 -0300

    Sidebar feito, criamos e estilizamos elementos do perfil do usuário.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import style from './SideBar.module.css';
 
 export function SideBar() {
@@ -6,12 +7,15 @@ export function SideBar() {
             <img className={style.cover} src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3'" alt="" />
             
             <div className={style.profile}>
-                <strong></strong>
+                <img src="https://avatars.githubusercontent.com/u/85140913?v=4" alt="" />
+
+                <strong>Gabriel Melo</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
